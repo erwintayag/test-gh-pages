dba2abfc4d83df0947545c963ea379d63db411bb
Erwin Tayag <99804510+erwintayag@users.noreply.github.com>
2024-08-19 12:11:50 +0800

    Update customScript.js

--- customScript.js
+++ customScript.js
@@ -1,9 +1,12 @@
 console.log("This is a custom script from GH...");
 debugger;
+
 // Hide the username and password fields initially
-document.getElementById("email").style.display = "block";
-document.getElementById("newPassword").style.display = "none";
-document.getElementById("reenterPassword").style.display = "none";
+function hidePasswordFields() {
+    document.getElementById("newPassword").style.display = "none";
+    document.getElementById("reenterPassword").style.display = "none";
+}
+
 
 // Show the username and password fields after verification
 function showPasswordFields() {
@@ -11,8 +14,16 @@ function showPasswordFields() {
     document.getElementById("reenterPassword").style.display = "block";
 }
 
+showPasswordFields();
+
 // Hook into the verification process
 document.getElementById("email_ver_but_verify").addEventListener("click", function() {
     // Assuming the verification is successful
     showPasswordFields();
 });
+
+document.getElementById("email_ver_but_edit").addEventListener("click", function() {
+    // Assuming the verification is successful
+    hidePasswordFields();
+});
+
