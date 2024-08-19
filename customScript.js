fd1913cb32667317b4f62eb5633b208b58d047a5
Erwin Tayag <99804510+erwintayag@users.noreply.github.com>
2024-08-19 13:20:14 +0800

    Update customScript.js

--- customScript.js
+++ customScript.js
@@ -1,29 +1,32 @@
 console.log("This is a custom script from GH...");
-debugger;
+// debugger;
 
-// Hide the username and password fields initially
-function hidePasswordFields() {
-    document.getElementById("newPassword").style.display = "none";
-    document.getElementById("reenterPassword").style.display = "none";
+// Toggle the details fields
+function fnSetDetailsFieldsDisplay(isDisplay) {
+    let display = isDisplay ? 'block' : 'none';
+    document.getElementById("newPassword").style.display = display;
+    document.getElementById("reenterPassword").style.display = display;
+    document.getElementById("givenName").style.display = display;
+    document.getElementById("surname").style.display = display;
+    document.getElementById("continue").style.display = display;
 }
 
+// // Show the username and password fields after verification
+// function showPasswordFields() {
+//     document.getElementById("newPassword").style.display = "block";
+//     document.getElementById("reenterPassword").style.display = "block";
+// }
 
-// Show the username and password fields after verification
-function showPasswordFields() {
-    document.getElementById("newPassword").style.display = "block";
-    document.getElementById("reenterPassword").style.display = "block";
-}
-
-showPasswordFields();
+fnSetDetailsFieldsDisplay(false);
 
 // Hook into the verification process
 document.getElementById("email_ver_but_verify").addEventListener("click", function() {
     // Assuming the verification is successful
-    showPasswordFields();
+    fnSetDetailsFieldsDisplay(true);
 });
 
 document.getElementById("email_ver_but_edit").addEventListener("click", function() {
     // Assuming the verification is successful
-    hidePasswordFields();
+    hidePasswordFields(false);
 });
 
