54d2d1611e76e5a8158207113bd535b243497363
Erwin Tayag <99804510+erwintayag@users.noreply.github.com>
2024-08-19 12:06:44 +0800

    Update customScript.js

--- customScript.js
+++ customScript.js
@@ -4,17 +4,15 @@ debugger;
 document.getElementById("email").style.display = "block";
 document.getElementById("newPassword").style.display = "none";
 document.getElementById("reenterPassword").style.display = "none";
-document.getElementById("continueButton").style.display = "none";
 
 // Show the username and password fields after verification
 function showPasswordFields() {
     document.getElementById("newPassword").style.display = "block";
     document.getElementById("reenterPassword").style.display = "block";
-    document.getElementById("continueButton").style.display = "block";
 }
 
 // Hook into the verification process
-document.getElementById("email_ver_but_send").addEventListener("click", function() {
+document.getElementById("email_ver_but_verify").addEventListener("click", function() {
     // Assuming the verification is successful
     showPasswordFields();
 });
