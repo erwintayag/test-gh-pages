24be40e4433e129b0f0387b3e1ba238ecea83656
Erwin Tayag <99804510+erwintayag@users.noreply.github.com>
2024-08-19 12:00:53 +0800

    Update customScript.js

--- customScript.js
+++ customScript.js
@@ -1,12 +1,10 @@
 console.log("This is a custom script from GH...");
 debugger;
 // Hide the username and password fields initially
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("email").style.display = "block";
-    document.getElementById("newPassword").style.display = "none";
-    document.getElementById("reenterPassword").style.display = "none";
-    document.getElementById("continueButton").style.display = "none";
-});
+document.getElementById("email").style.display = "block";
+document.getElementById("newPassword").style.display = "none";
+document.getElementById("reenterPassword").style.display = "none";
+document.getElementById("continueButton").style.display = "none";
 
 // Show the username and password fields after verification
 function showPasswordFields() {
