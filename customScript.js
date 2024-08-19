3ba5579b34bf9903c835dfc368b82ed6aeb4889b
Erwin Tayag <99804510+erwintayag@users.noreply.github.com>
2024-08-19 10:53:02 +0800

    Update customScript.js

--- customScript.js
+++ customScript.js
@@ -1,20 +1,26 @@
-// Hide the username and password fields initially
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("email").style.display = "block";
-    document.getElementById("newPassword").style.display = "none";
-    document.getElementById("reenterPassword").style.display = "none";
-    document.getElementById("continueButton").style.display = "none";
-});
+$(document).ready(function () {
+    console.log("this is a custom script from GH...");
+    // Hide the username and password fields initially
+    document.addEventListener("DOMContentLoaded", function() {
+        document.getElementById("email").style.display = "block";
+        document.getElementById("newPassword").style.display = "none";
+        document.getElementById("reenterPassword").style.display = "none";
+        document.getElementById("continueButton").style.display = "none";
+    });
 
-// Show the username and password fields after verification
-function showPasswordFields() {
-    document.getElementById("newPassword").style.display = "block";
-    document.getElementById("reenterPassword").style.display = "block";
-    document.getElementById("continueButton").style.display = "block";
+    // Show the username and password fields after verification
+    function showPasswordFields() {
+        document.getElementById("newPassword").style.display = "block";
+        document.getElementById("reenterPassword").style.display = "block";
+        document.getElementById("continueButton").style.display = "block";
+    }
+    
+    // Hook into the verification process
+    document.getElementById("verifyButton").addEventListener("click", function() {
+        // Assuming the verification is successful
+        showPasswordFields();
+    });
 }
 
-// Hook into the verification process
-document.getElementById("verifyButton").addEventListener("click", function() {
-    // Assuming the verification is successful
-    showPasswordFields();
-});
+
+
