cd201c0f4f146788dff71ea5955bbdd5439bee25
Erwin Tayag <99804510+erwintayag@users.noreply.github.com>
2024-09-13 11:07:29 +0800

    Update customScript.js

--- customScript.js
+++ customScript.js
@@ -1,3 +1,7 @@
+/*
+ * Original: https://github.com/stevenxzhou-zz/B2C-Samples/blob/master/Email-Verification-Automatic-Redirect.js
+ */
+
 var applyB2CAddOns = function() {
 
     var verifyCodeBtn = document.querySelector(".verifyButton");
@@ -31,86 +35,84 @@ var applyB2CAddOns = function() {
 
     var failedToRedirect = false;
 
-    // Overriding following two methods so that we can detect the failure calling these two methods.
-    $element.onError = function(code, message, isSendingQuietly) {
-        if (isSendingQuietly) {
-            $diags.sendQuietDiagnostics(code, message);
-        } else {
-            $diags.sendDiagnostics(code, message);
-        }
-        failedToRedirect = true;
-        return false;
-    }
-
-    // Sets error message and shows it to the user.
-    $element.setAndShowErrorMessage = function (id, msg) {
-        var $id = $("#" + id);
-
-        if (msg) {
-            $id.text(msg);
+    if ($element) {
+        // Overriding following two methods so that we can detect the failure calling these two methods.
+        $element.onError = function (code, message, isSendingQuietly) {
+            if (isSendingQuietly) {
+                $diags.sendQuietDiagnostics(code, message);
+            } else {
+                $diags.sendDiagnostics(code, message);
+            }
+            failedToRedirect = true;
+            return false;
         }
 
-        // Add the aria attributes and tabindex allowing the message to receive focus
-        $id.attr({ "role": "alert", "aria-live": "polite", "aria-hidden": "false", "tabindex": "1" }).css("display", "block");
+        // Sets error message and shows it to the user.
+        $element.setAndShowErrorMessage = function (id, msg) {
+            var $id = $("#" + id);
 
-        failedToRedirect = true;
+            if (msg) {
+                $id.text(msg);
+            }
+
+            // Add the aria attributes and tabindex allowing the message to receive focus
+            $id.attr({ "role": "alert", "aria-live": "polite", "aria-hidden": "false", "tabindex": "1" }).css("display", "block");
+
+            failedToRedirect = true;
+        }
     }
-
+    
     // Adding auto submission once found it is a email verification page.
-    verifyCodeBtn.onclick = function() {
+    if (verifyCodeBtn) {
+        verifyCodeBtn.onclick = function() {
 
-        failedToRedirect = false;
-        readyToRedirect = false;
-
-        // Continue the page once email is validated.
-        var verifyInterval = setInterval(function() {
-            if ($element.verificationInfo.email.confirmation.success) {
-                clearInterval(verifyInterval);
-                $element.verify();
-                cancelBtn.style.display = "none";
-                changeEmailBtn.style.display = "none";
-            }
-        }, 50);
-
-        // Confirm the page for redirect once server side validate is passed.
-        var confirmInterval = setInterval(function() {
-            if (readyToRedirect) {
-                clearInterval(confirmInterval);
-                clearInterval(failedInterval);
-                $i2e.redirectToServer("confirmed");
-            }
-        }, 50);
-
-        // Confirm the page for redirect once server side validate is passed.
-        var failedInterval = setInterval(function() {
-            if (failedToRedirect) {
-                clearInterval(failedInterval);
-                cancelBtn.style.display = "inline-block";
-                changeEmailBtn.style.display = "inline-block";
-            }
-        }, 50);
+            failedToRedirect = false;
+            readyToRedirect = false;
+    
+            // Continue the page once email is validated.
+            var verifyInterval = setInterval(function() {
+                if ($element.verificationInfo.email.confirmation.success) {
+                    clearInterval(verifyInterval);
+                    $element.verify();
+                    cancelBtn.style.display = "none";
+                    changeEmailBtn.style.display = "none";
+                }
+            }, 50);
+    
+            // Confirm the page for redirect once server side validate is passed.
+            var confirmInterval = setInterval(function() {
+                if (readyToRedirect) {
+                    clearInterval(confirmInterval);
+                    clearInterval(failedInterval);
+                    $i2e.redirectToServer("confirmed");
+                }
+            }, 50);
+    
+            // Confirm the page for redirect once server side validate is passed.
+            var failedInterval = setInterval(function() {
+                if (failedToRedirect) {
+                    clearInterval(failedInterval);
+                    cancelBtn.style.display = "inline-block";
+                    changeEmailBtn.style.display = "inline-block";
+                }
+            }, 50);
+        }
     }
+    
 
     function decodeDefaultValuesFromQueryParams() {
-        // inputs
+        // page controls
         var firstName = document.querySelector("#givenName");
         var lastName = document.querySelector("#surname");
         var countryCode = document.querySelector("#inputCountryCode");
         var phoneNumber = document.querySelector("#inputPhoneNumber");
     
+        // get the query params
+        // window.SETTINGS - https://learn.microsoft.com/en-us/azure/active-directory-b2c/javascript-and-page-layout?pivots=b2c-custom-policy
         var remoteUrl = new URL(window.SETTINGS.remoteResource);
         var queryParams = remoteUrl.searchParams;
-        // var firstNameValue = queryParams.get('first_name');
-        // if (firstNameValue) {
-        //     // firstName.value = decodeURIComponent(firstNameValue);
-        //     console.log("test:" + firstNameValue);
-        //     firstName.value = decodeURIComponent(firstNameValue);
-        // }
 
         if (queryParams) {
-            // queryParams.has('first_name') ? firstName.value = decodeURIComponent(queryParams.get('first_name')) : null;
-            // queryParams.has('last_name') ? lastName.value = decodeURIComponent(queryParams.get('last_name')) : null;
-            // queryParams.has('phone_number') ? phoneNumber.value = decodeURIComponent(queryParams.get('phone_number')) : null;
             try {
                 if (firstName && queryParams.has('first_name')) {
                      firstName.value = atob(queryParams.get('first_name'));
@@ -131,26 +133,6 @@ var applyB2CAddOns = function() {
                 console.error("Error while decoding query params: " + error);
             }
         }
-    
-        // if (firstName) {
-        //     firstName.value = atob(firstName.value);
-        // }
-
-        // if (lastName) {
-        //     lastName.value = atob(lastName.value);
-        // }
-
-        // if (countryCode) {
-        //     countryCode.value = atob(countryCode.value);
-        //     // countryCode is dropdown
-        //     var countryCodeDropdown = document.querySelector("#inputCountryCode");
-        //     countryCodeDropdown.selectedIndex = countryCodeDropdown.querySelector("option[value='" + countryCode.value + "']").index;
-
-        // }
-
-        // if (phoneNumber) {
-        //     phoneNumber.value = atob(phoneNumber.value);
-        // }
     }
 };
 
